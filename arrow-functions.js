243243fdf786ad4ea40335c3078677a9ec3fb7e4
Tonywilson1211 <ant.wilson@hotmail.com>
2023-04-01 14:39:27 +0000

    commit

--- arrow-functions.js
+++ arrow-functions.js
@@ -4,11 +4,37 @@
  */
 
 // Vanilla JavaScript Function
+function addTwoNumbers(a, b) {
+    return a + b
+}
+let sum = addTwoNumbers(3,5)
+console.log(sum);
 
 // Arrow Function With Parameters
+const addTwoNumbersOne = (a, b) => {
+    // Code Block
+    return a + b
+}
+let sums = addTwoNumbersOne(2,5)
+console.log(sums);
 
 // Single Line Arrow Function With Parameters
+    // const addTwoNumbers2 = (a,b) => (a + b); also valid
+const addTwoNumbers2 = (a,b) => a + b;
 
 // Implicit Returns
+const saySomething = message => console.log(message);
+saySomething('Hello!');
+
+const sayHello = () => console.log('hello again');
+sayHello();
+
 
 // Returning Multiple Lines
+
+const returnMultpleLines = () => (
+    `<p>
+    This is a multiline string!
+    </p>`
+)
+console.log(returnMultpleLines())
