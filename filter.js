3d3adbc6c84a745c52c0adb25aef5122db14c676
Tonywilson1211 <ant.wilson@hotmail.com>
2023-04-09 16:36:00 +0000

    reduce.js

--- filter.js
+++ filter.js
@@ -13,5 +13,110 @@ let names = ['mike','jill', 'matt','jane', 'tony'];
 let mNames = names.filter(name => name.charAt(0) === 'm') // iterate through names and look for first element 'm'. 0 = first element.
 console.log(mNames) // mike, matt
 
+// Simple Filtering
+const people = [
+    {
+      name: 'Michael',
+      age: 23,
+    },
+    {
+      name: 'Lianna',
+      age: 16,
+    },
+    {
+      name: 'Paul',
+      age: 18,
+    },
+  ];
+
+// qualify by age
+const oldEnough = people.filter(person => person.age >= 21);
+console.log(oldEnough)
+
+// look for a name
+const paul = people.filter(p => p.name === "Paul");
+console.log(paul)
+
+ 
+  
+// Complex Filtering
+const students = [
+{
+    id: 1,
+    name: 'Mark',
+    profession: 'Developer',
+    skills: [
+    { name: 'javascript', yrsExperience: 1 },
+    { name: 'html', yrsExperience: 5 },
+    { name: 'css', yrsExperience: 3 },
+    ]
+},
+{
+    id: 2,
+    name: 'Ariel',
+    profession: 'Developer',
+    skills: [
+    { name: 'javascript', yrsExperience: 0 },
+    { name: 'html', yrsExperience: 4 },
+    { name: 'css', yrsExperience: 2 },
+    ]
+},
+{
+    id: 3,
+    name: 'Jason',
+    profession: 'Designer',
+    skills: [
+    { name: 'javascript', yrsExperience: 1 },
+    { name: 'html', yrsExperience: 1 },
+    { name: 'css', yrsExperience: 5 },
+    ]
+},
+];
+
+// complex filter to show which student has 5 or more years experience
+const candidates = students.filter(student => {
+    let strongSkills = student.skills.filter(skill => skill.yrsExperience >= 5);
+    return strongSkills.length > 0;
+})
+console.log(candidates)
+
+
+const has5yearsExp = skill => skill.yrsExperience >= 1; // main filter function - equal to or more years experience
+const hasStrongSkills = student => student.skills.filter(has5yearsExp).length > 0; // takes the main function and creates a new function to pass through the student skills object
+const candidate = students.filter(hasStrongSkills); // defines candidate and passes the second function (which also contains the first function wrapped in it) to then pass the result
+console.log(candidate) // this helps keep code neat and easy to read
+
+
+// challenge - find names with highest scoring maths results.
+let studentS = [
+    {
+        name: 'John',
+        subjects: ['maths', 'english', 'cad'],
+        teacher: {maths: 'Harry', english: 'Joan', cad: 'Paul'},
+        results: {maths: 90, english: 75, cad: 87},
+    },
+    {
+        name: 'Emily',
+        subjects: ['science', 'english', 'art'],
+        teacher: {science: 'Iris', english: 'Joan', art: 'Simon'},
+        results: {science: 93, english: 80, art: 95},
+    },
+    {
+        name: 'Adam',
+        subjects: ['science', 'maths', 'art'],
+        teacher: {science: 'Iris', maths: 'Harry', art: 'Simon'},
+        results: {science: 84, maths: 97, art: 95},
+    },
+    {
+        name: 'Fran',
+        subjects: ['science', 'english', 'art'],
+        teacher: {science: 'Iris', english: 'Joan', art: 'Simon'},
+        results: {science: 67, english: 87, art: 95},
+    }
+];
+
+const topMaths = studentS.filter(student => student.results.maths >= 90);
+console.log(topMaths)
+
 
 
--- reduce.js
+++ reduce.js
@@ -5,6 +5,96 @@
 */
 
 const nums = [0,1,2,3,4];
-let sum = nums.reduce((acc, curr) => acc + curr, 0);
+let sum = nums.reduce((acc, curr) => acc + curr, 0); // acc = accumulator and curr = currentValue
 console.log(sum); //10
 
+const numS = [0,1,2,3,4];
+let sumS = nums.reduce((acc, curr) => {
+    console.log(
+        "Accumulator: ", acc,
+        "Current Value: ", curr,
+        "Total: ", acc + curr
+    );
+    return acc + curr;
+}, 0) // the zero explicity tells the function to start with the first value. If blank, it would be 0 , 1 , 1 aka initial value
+
+
+// Summing an array of numbers:
+
+
+const teamMembers = [
+    {
+      name: 'Andrew',
+      profession: 'Developer',
+      yrsExperience: 5
+    },
+    {
+      name: 'Ariel',
+      profession: 'Developer',
+      yrsExperience: 7
+    },
+    {
+      name: 'Michael',
+      profession: 'Designer',
+      yrsExperience: 1
+    },
+    {
+      name: 'Kelly',
+      profession: 'Designer',
+      yrsExperience: 3
+    }
+  ];
+  
+  // Totaling a specific object property
+  const totalExperience = teamMembers.reduce((acc, curr) => acc + curr.yrsExperience, 0);
+  console.log(totalExperience)
+  
+  // Grouping by a property, and totaling it too
+  let experienceByProfession = teamMembers.reduce((acc, curr) => {
+    let key = curr.profession;
+    if (!acc[key]) {
+        acc[key] = curr.yrsExperience;
+    } else {
+        acc[key] += curr.yrsExperience;
+    }
+    return acc;
+  }, {})
+  console.log(experienceByProfession)
+  //{Developer: 12, Designer: 4}
+
+// reduce challenge
+
+let students = [
+  {
+      name: 'John',
+      subjects: ['maths', 'english', 'cad'],
+      teacher: {maths: 'Harry', english: 'Joan', cad: 'Paul'},
+      results: {maths: 90, english: 75, cad: 87},
+  },
+  {
+      name: 'Emily',
+      subjects: ['science', 'english', 'art'],
+      teacher: {science: 'Iris', english: 'Joan', art: 'Simon'},
+      results: {science: 93, english: 73, art: 95},
+  },
+  {
+      name: 'Adam',
+      subjects: ['science', 'maths', 'art'],
+      teacher: {science: 'Iris', maths: 'Harry', art: 'Simon'},
+      results: {science: 93, english: 88, maths: 97, art: 95},
+  },
+  {
+      name: 'Fran',
+      subjects: ['science', 'english', 'art'],
+      teacher: {science: 'Iris', english: 'Joan', art: 'Simon'},
+      results: {science: 93, english: 87, art: 95},
+  }
+];
+
+const biggest = students.reduce((acc, curr) => {
+if (curr.results.english > acc.max) {
+  return { name: curr.name, max: curr.results.english };
+}
+return acc;
+}, { name: '', max: 0 });
+console.log(biggest);
