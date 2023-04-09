2d8aa28a856e4756941316eccb706cb09808bf13
Tonywilson1211 <ant.wilson@hotmail.com>
2023-04-09 11:10:07 +0000

    map challenge

--- map.js
+++ map.js
@@ -81,3 +81,36 @@ const studentsWithIdsAndNames = students.map(student => {
   });
   
   console.log(studentsWithIdsAndNames);
+
+  // call certain part of object
+
+  let pupils = [
+    {
+        name: 'John',
+        subjects: ['maths', 'english', 'cad'],
+        teacher: {maths: 'Harry', english: 'Joan', cad: 'Paul'},
+        results: {maths: 90, english: 75, cad: 87},
+    },
+    {
+        name: 'Emily',
+        subjects: ['science', 'english', 'art'],
+        teacher: {science: 'Iris', english: 'Joan', art: 'Simon'},
+        results: {science: 93, english: 80, art: 95},
+    },
+    {
+        name: 'Adam',
+        subjects: ['science', 'maths', 'art'],
+        teacher: {science: 'Iris', maths: 'Harry', art: 'Simon'},
+        results: {science: 93, maths: 77, art: 95},
+    },
+    {
+        name: 'Fran',
+        subjects: ['science', 'english', 'art'],
+        teacher: {science: 'Iris', english: 'Joan', art: 'Simon'},
+        results: {science: 93, english: 87, art: 95},
+    }
+];
+
+let [john, ...rest] = pupils.map(({name, results}) => [name, results]);
+console.log(john);
+console.log(rest);
